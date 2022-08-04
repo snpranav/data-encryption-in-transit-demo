50879a98625794a7c34f3fbbd3d852f140bd9acb
snpranav <contact@snpranav.com>
2022-08-04 19:37:47 +0000

    misc: encrypted -> decrypted

--- components/DecryptedData.js
+++ components/DecryptedData.js
@@ -5,7 +5,7 @@ const DecryptedDataComponent = ({ data, dbName }) => {
 
     return (
         <div className="flex flex-col items-center justify-center mt-12">
-            <h1 className="text-2xl font-semibold">Encrypted Data <span className="text-orange-500">NOT</span> Stored In {dbName} DB </h1>
+            <h1 className="text-2xl font-semibold">Decrypted Data <span className="text-orange-500">NOT</span> Stored In {dbName} DB </h1>
             <CopyBlock
                 language="json"
                 text={data}
